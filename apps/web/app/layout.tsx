7c21cfc42d8c7d638804e680a7c50281e5ba0785
TEN_GITHUB_DUNG <EMAIL_GITHUB_DUNG>
2026-03-27 11:03:05 +0700

    fix: load google analytics with next script

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
+import Script from "next/script";
 
 const plusJakartaSans = Plus_Jakarta_Sans({
   subsets: ["latin"],
@@ -21,6 +22,15 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
   return (
     <html lang="en">
       <body className={`${plusJakartaSans.variable}`}>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-8FRPDTZV9Y" strategy="afterInteractive" />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-8FRPDTZV9Y');
+          `}
+        </Script>
         <div id="main-content">{children}</div>
       </body>
     </html>
